b21a5a0f1de15ea97818b7d7fac2d7d24fb99460
Cody Wyers <cody@wyers.dev>
2021-09-28 13:38:20 -0500

    adding locations lib

---
+++ libs/locations/src/lib/locations.spec.ts
@@ -0,0 +1,7 @@
+import { locations } from './locations';
+
+describe('locations', () => {
+  it('should work', () => {
+    expect(locations()).toEqual('locations');
+  });
+});
---
+++ libs/locations/src/lib/locations.ts
@@ -0,0 +1,3 @@
+export function locations(): string {
+  return 'locations';
+}
